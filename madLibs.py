d27156f90aba84310a563be06aaded3e152369ec
Jorge Gallegos <gallegosjorge403@gmail.com>
2019-09-03 11:33:35 -0700

    Finished story

--- madLibs.py
+++ madLibs.py
@@ -1,5 +1,5 @@
 # Story from: http://www.redkid.net/cgi-bin/madlibs/videogames.pl
-"""I love to {} {} video games. I can play them day and
+story = """I love to {} {} video games. I can play them day and
 {}! My mom and {} are not too happy with my
 {} so much time in front of the television {}.
 Although Dad believes that these {} games help children
@@ -46,3 +46,13 @@ body1 = user_input("Part of Body: ")
 body2 = user_input("Part of Body: ")
 
 num = user_input("Number: ")
+
+adv = user_input("Adverb: ")
+
+# print("I love to {} {} video games. I can play them day and {}! My mom and {} are not too happy with my {} so much time in front of the television {}.".format(adv, verb1, nouns[0], nouns[1], verb2, nouns[2]))
+# print("Although Dad believes that these {} games help children develop hand-{} coordination and improve their learning {}, he also seems to think they have {} side effects on one's {}.".format(adjectives[0], body1, plural_nouns[0], adjectives[1], body2))
+# print("Both of my {} think this is due to a {} use of violence in the majority of the {}. Finally, we all arrived at a {} compromise: After dinner I can play {} hours of video games, provided I help clear the {} and wash the {}.".format(plural_nouns[1], adjectives[2], plural_nouns[2], adjectives[3], num, nouns[3], plural_nouns[3]))
+
+story = story.format(adv, verb1, nouns[0], nouns[1], verb2, nouns[2],adjectives[0], body1, plural_nouns[0], adjectives[1], body2,plural_nouns[1], adjectives[2], plural_nouns[2], adjectives[3], num, nouns[3], plural_nouns[3])
+
+print(story)
